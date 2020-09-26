f2ca2d24810673374e7c30793830ccc986303d92
saumya verma <saumya.verma@ourbus.com>
2020-09-26 15:32:11 +0530

    Updates

---
+++ precache-manifest.9e759f843c9ee3d834f00d911d12cb79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958f35b257889858bd9a83a9ccc3748a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "395f4da40572ece87e22",
+    "url": "/static/css/main.857b2ba1.chunk.css"
+  },
+  {
+    "revision": "1f12c135eda365691bbb",
+    "url": "/static/js/2.3b1c8fad.chunk.js"
+  },
+  {
+    "revision": "abe08a1506f951fe89cb6492063e650e",
+    "url": "/static/js/2.3b1c8fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "395f4da40572ece87e22",
+    "url": "/static/js/main.c1b99d92.chunk.js"
+  },
+  {
+    "revision": "202bca30018d23b019aa",
+    "url": "/static/js/runtime-main.059db505.js"
+  }
+]);
